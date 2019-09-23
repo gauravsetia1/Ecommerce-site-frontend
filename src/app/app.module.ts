434e085a724d21000257db58f95a723f4c16042f
Gaurav Setia <gauravsetia1999@gmail.com>
2019-09-23 14:43:18 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { UserCartComponent } from './user-cart/user-cart.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    HomePageComponent,
+    NavBarComponent,
+    ProductDetailsComponent,
+    ProductListComponent,
+    UserCartComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
