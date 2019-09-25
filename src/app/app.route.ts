8a00da32af150bc1baaa7c9f87ff7a606394cec4
Gaurav Setia <gauravsetia1999@gmail.com>
2019-09-25 10:54:58 +0530

    routing is working

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -1,8 +1,13 @@
 import {Routes} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
 import {UserCartComponent} from './user-cart/user-cart.component';
+import {LoginComponent} from './login/login.component';
+import {SignUpComponent} from './sign-up/sign-up.component';
 
 export const MAIN_ROUTES: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomePageComponent},
-  {path: 'cart', component: UserCartComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignUpComponent},
+  {path: 'mycart', component: UserCartComponent},
 ];
