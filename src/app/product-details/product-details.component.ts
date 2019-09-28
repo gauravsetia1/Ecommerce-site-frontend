87921c6e04ba9eb4e5e981a4359af41a75d2b7bc
Gaurav Setia <gauravsetia1999@gmail.com>
2019-09-28 22:55:18 +0530

    product detail created

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,6 +10,12 @@ export class ProductDetailsComponent implements OnInit {
   productId;
   constructor(private router: Router, private route: ActivatedRoute) { }
 
+  productDetail = {
+    name: 'Iphone XS',
+    image: './assets/images/iphone.PNG',
+    spec: '3GB|64GB|3000mAH',
+    price: '1999'
+  };
   ngOnInit() {
     this.route.paramMap.subscribe((params: ParamMap) => {
       const id = parseInt(params.get('id'));
